c881208bc67fb7e994b8418d950439a54249a647
unknown <leanhtuan131104@gmail.com>
2025-12-16 17:22:28 +0700

    update: update ui my-appointment of patient

--- services/api/axios-config.ts
+++ services/api/axios-config.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL || 'https://comprehensive-pdt-barriers-installation.trycloudflare.com/api',
+  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL || 'https://relates-why-produces-philip.trycloudflare.com/api',
   timeout: 10000,
   headers: {
     'Content-Type': 'application/json',
